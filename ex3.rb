33503371f3d723e875238e4fa668336148c8ff25
JTong <eldertong@gmail.com>
2016-08-18 14:20:44 -0400

    Initial ex3.rb push

---
+++ ex3.rb
@@ -0,0 +1,35 @@
+
+# types text about counting chickens
+puts "I will no count my chickens:"
+
+# calculates the number of hens and roosters
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# types text about counting eggs
+puts "Now I will count the eggs:"
+
+# calculates the number of eggs.  Don't know why this equation is so complicated.
+# remember to ask in class
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# types out text regarding the question
+puts "Is it true that 3 + 2 < 5 - 7"
+
+# actually has the code answer the question
+puts 3 + 2 < 5 - 7
+
+# asks the question in text then uses code to find the answer
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+# types text seen below
+puts "Oh, that's why it's false."
+
+# types text seen below
+puts "How about some more."
+
+# asks the question in text then uses code to find the answer
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
