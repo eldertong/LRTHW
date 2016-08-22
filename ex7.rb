430ca20bdab67506ed31d7cd6a8b0be9f06d52fe
JTong <eldertong@gmail.com>
2016-08-22 16:37:06 -0400

    init ex7

---
+++ ex7.rb
@@ -0,0 +1,27 @@
+# displays text
+puts "Mary had a little lamb."
+# displays text
+puts "Its fleece was white as #{'snow'}."
+# displays text
+puts "And everywhere that Mary went."
+# displays text
+puts "." * 10  # what'd that do? this put 10 periods
+
+# defines the following
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do? 
+# this puts the defined characters above in a string order that spells cheeseburger
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
