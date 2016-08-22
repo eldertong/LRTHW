031c62b33e8cf2005bc710010875c530e9332cc7
JTong <eldertong@gmail.com>
2016-08-22 11:43:44 -0400

    ex5 init

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+name = 'Jeremy E Tong'
+age = 33 # not a lie in 2016
+height = (73 * 2.54) # inches now in cm
+weight = (260 * 0.453592) # lbs now in kg
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} cintimeters tall."
+puts "He's #{weight} kilograms heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
