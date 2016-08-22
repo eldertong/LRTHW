e908ec4c2ed0c812586b0646e1c72ab04e78a95e
JTong <eldertong@gmail.com>
2016-08-22 12:04:14 -0400

    init ex6

---
+++ ex6.rb
@@ -0,0 +1,36 @@
+# number os people is actually 2 but in binary 2 is 10
+types_of_people = 10
+# this is a string unsing the above variable
+x = "There are #{types_of_people} types of people."
+# defines binary
+binary = "binary"
+# defines do not to don't  someone does not like contractions
+do_not = "don't"
+# sting using the above variables
+y = "Those who know #{binary} and those who #{do_not}."
+
+# prints x and y string
+puts x
+puts y
+
+# prints above string in a more conversational manner
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+# defines halarious to false but i set it to true
+hilarious = true
+# string with above definition
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# prints above string
+puts joke_evaluation
+
+# defines w and e
+w = "This is the left side of..."
+e = 'a string with a right side.'
+
+# prints w and e
+puts w + e
+
+# w + e makes a sentence out of the 2 definitions
+# using ' instead of " works so long as you do not have a contraction'
